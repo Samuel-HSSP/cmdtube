12987f9d51b77b52482d69d2487e73a167f5eeef
Samuel Ayomide Ogunleke <72163771+Samuel-HSSP@users.noreply.github.com>
2022-02-06 01:51:14 +0100

    Update __init__.py

--- src/cmdtube/__init__.py
+++ src/cmdtube/__init__.py
@@ -1,5 +1,5 @@
 """
-CmdTube: A Python cli dedicated to all programmers who learn from YouTube.
+CmdTube: A Python CLI dedicated to all programmers who learn from YouTube.
 """
 __title__ = "cmdtube"
 __author__ = "Samuel Ogunleke"
